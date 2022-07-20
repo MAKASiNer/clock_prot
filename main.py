1ab45bf7cbc07244295f8c14eb9525a8bc13e953
MAKASiNer <max.pavlutenkof@gmail.com>
2022-07-20 03:39:22 +0300

    master

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def str2arr(time_str: str, zero: list[float, float] = ZERO) -> Array:
     return tiles
 
 
-def step(old: Array, new: Array, step: float):
+def render(old: Array, new: Array, step: float):
     step = min(1, step)
     res = old.copy()
     for y in range(min(len(old), len(new))):
@@ -46,56 +46,73 @@ def step(old: Array, new: Array, step: float):
     return res
 
 
-pygame.init()
+def main():
+    pygame.init()
 
-size = width, height = 900, 600
-black = 255, 255, 255
+    # размеры поля
+    size = 900, 600
 
-screen = pygame.display.set_mode(size)
+    # цвет фона
+    bg_clr = 255, 255, 255
 
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-    screen.fill(black)
+    # радиуc циферблата
+    r, d = 20, 40
+            
+    # размер часовой и минутной стрелок в процентах от радиуса
+    h, m = 0.99, 0.99
 
-    def dt_time2str(time) -> str:
-        h = str(time.hour).rjust(2, '0')
-        m = str(time.minute).rjust(2, '0')
-        return f'{h}:{m}'
+    # за сколько секунд циферблат перестроит цифру (от 1 до 59)
+    t = 10
+    
+    screen = pygame.display.set_mode(size)
 
-    now = dt.datetime.today()
-    data_prev = str2arr(dt_time2str(now - dt.timedelta(minutes=1)))
-    data_next = str2arr(dt_time2str(now))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        screen.fill(bg_clr)
 
-    data = step(data_prev, data_next, now.second / 59)
-    print(now)
+        def dt_time2str(time) -> str:
+            h = str(time.hour).rjust(2, '0')
+            m = str(time.minute).rjust(2, '0')
+            return f'{h}:{m}'
 
-    for y in range(len(data)):
-        for x in range(len(data[y])):
-            a, b = data[y][x]
+        now = dt.datetime.today()
+        data_prev = str2arr(dt_time2str(now - dt.timedelta(minutes=1)))
+        data_next = str2arr(dt_time2str(now))
 
-            r, d = 20, 40
-            h, m = 0.99, 0.99
+        data = render(data_prev, data_next, now.second / max(1, min(t, 59)))
 
-            pygame.draw.circle(screen,
-                               center=[x * d + r, y * d + r],
-                               radius=r,
-                               color="#9f9f9f",
-                               width=1)
+        print(now)
 
-            pygame.draw.line(screen,
-                             start_pos=[(x + 0.5) * d, (y + 0.5) * d],
-                             end_pos=[(x + 0.5) * d + r * cos(a * pi) * h,
-                                      (y + 0.5) * d + r * sin(-a * pi) * h],
-                             color="#7f0000",
-                             )
+        for y in range(len(data)):
+            for x in range(len(data[y])):
+                a, b = data[y][x]
+ 
+                d = 2 * r
 
-            pygame.draw.line(screen,
-                             start_pos=[(x + 0.5) * d, (y + 0.5) * d],
-                             end_pos=[(x + 0.5) * d + r * cos(b * pi) * h,
-                                      (y + 0.5) * d + r * sin(-b * pi) * h],
-                             color="#00007f",
-                             )
+                pygame.draw.circle(screen,
+                                center=[x * d + r, y * d + r],
+                                radius=r,
+                                color="#9f9f9f",
+                                width=1)
 
-    pygame.display.flip()
+                pygame.draw.line(screen,
+                                start_pos=[(x + 0.5) * d, (y + 0.5) * d],
+                                end_pos=[(x + 0.5) * d + r * cos(a * pi) * h,
+                                        (y + 0.5) * d + r * sin(-a * pi) * h],
+                                color="#7f0000",
+                                )
+
+                pygame.draw.line(screen,
+                                start_pos=[(x + 0.5) * d, (y + 0.5) * d],
+                                end_pos=[(x + 0.5) * d + r * cos(b * pi) * m,
+                                        (y + 0.5) * d + r * sin(-b * pi) * m],
+                                color="#00007f",
+                                )
+
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    main()
